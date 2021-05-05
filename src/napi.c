430c865d829c288ea52d9a40f70ffc6556672d36
anastasia <anastasia.nekrasova@jetbrains.com>
2021-05-05 15:54:32 +0300

    Update

--- src/napi.c
+++ src/napi.c
@@ -11,10 +11,10 @@ napi_value first_mode_fn(napi_env env, napi_callback_info info) {
 }
 
 napi_value pwd_fn(napi_env env, napi_callback_info info) {
-  napi_value napi_result;
-  char *result = pwd();
-  napi_create_string_utf8(env, result, -1, &napi_result);
-  return napi_result;
+//  napi_value napi_result;
+//  char *result = pwd();
+//  napi_create_string_utf8(env, result, -1, &napi_result);
+//  return napi_result;
 }
 
 napi_value ls_fn(napi_env env, napi_callback_info info) {
