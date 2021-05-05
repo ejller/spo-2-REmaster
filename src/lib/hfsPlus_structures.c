d8fb66b5222f4891decfcf5037e71568721bbc0f
anastasia <anastasia.nekrasova@jetbrains.com>
2021-05-05 14:59:30 +0300

    Update

--- src/lib/hfsPlus_structures.c
+++ src/lib/hfsPlus_structures.c
@@ -50,15 +50,15 @@ void reverseHFSPlusVolumeHeader(HFSPlusVolumeHeader *s) {
     s->dataClumpSize = bswap_32(s->dataClumpSize);
     s->nextCatalogID = bswap_32(s->nextCatalogID);
     s->writeCount = bswap_32(s->writeCount);
-//    s->encodingsBitmap = bswap_64(s->encodingsBitmap);
-//    for (int i = 0; i < 8; i++) {
-//        s->finderInfo[i] = bswap_32(s->finderInfo[i]);
-//    }
-//    reverseHFSPlusForkData(&s->allocationFile);
-//    reverseHFSPlusForkData(&s->attributesFile);
-//    reverseHFSPlusForkData(&s->extentsFile);
-//    reverseHFSPlusForkData(&s->catalogFile);
-//    reverseHFSPlusForkData(&s->startupFile);
+    s->encodingsBitmap = bswap_64(s->encodingsBitmap);
+    for (int i = 0; i < 8; i++) {
+        s->finderInfo[i] = bswap_32(s->finderInfo[i]);
+    }
+    reverseHFSPlusForkData(&s->allocationFile);
+    reverseHFSPlusForkData(&s->attributesFile);
+    reverseHFSPlusForkData(&s->extentsFile);
+    reverseHFSPlusForkData(&s->catalogFile);
+    reverseHFSPlusForkData(&s->startupFile);
 }
 
 void reverseBTNodeDescriptor(BTNodeDescriptor *s) {
