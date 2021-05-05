3a91a70c9073841913535e4179185dffd39da7d6
anastasia <anastasia.nekrasova@jetbrains.com>
2021-05-05 14:38:33 +0300

    Update

--- src/napi.c
+++ src/napi.c
@@ -11,9 +11,14 @@ napi_value first_mode_fn(napi_env env, napi_callback_info info) {
 }
 
 napi_value open_file_system_fn(napi_env env, napi_callback_info info) {
-  char *path =(char*)malloc(2);
-  napi_value argv[1];
-  napi_get_value_string_utf8(env, argv[0], -1, &path);
+  char *path;
+  size_t str_size;
+  napi_get_value_string_utf8(env, argv[0], NULL, 0, &str_size);
+  str_size += 1;
+
+  path = (char*)calloc(str_size + 1, sizeof(char));
+  size_t str_size_read;
+  napi_get_value_string_utf8(env, argv[0], path, str_size, &str_size_read);
 
   napi_value napi_result;
   int result = openFileSystem(path);
