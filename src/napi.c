f8a563b51311c66e0ac465212906f13cbec80c42
anastasia <anastasia.nekrasova@jetbrains.com>
2021-05-05 22:36:59 +0300

    Update

--- src/napi.c
+++ src/napi.c
@@ -44,9 +44,9 @@ napi_value open_file_system_fn(napi_env env, napi_callback_info info) {
     napi_throw_error(env, NULL, "Failed to parse arguments");
   }
 
-  char PathName[4];
+  char PathName[20];
   size_t pathResult;
-  status = napi_get_value_string_utf8(env, argv[0], PathName, 4, &pathResult);
+  status = napi_get_value_string_utf8(env, argv[0], PathName, 20, &pathResult);
 
   if (status != napi_ok) {
     napi_throw_error(env, NULL, "Invalid number was passed as argument");
